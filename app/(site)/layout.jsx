586d9f3ef471bbf39d2634ea3ca493a3e9412389
Mufaddal Aurangabadi <mufaddalaurangabadi@gmail.com>
2025-03-26 22:24:09 +0530

    main 1

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -18,13 +18,25 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != "dataLayer" ? "&l=" + l : "";
+            j.async = true;
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, "script", "dataLayer", "GTM-W2CHMJNV")`}
+        </Script>
+        {/* <Script
           id="google-analytics"
           strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W2CHMJNV');`}</Script>
+})(window,document,'script','dataLayer','GTM-W2CHMJNV');`}</Script> */}
       </head>
       <body className={inter.className}>
         <noscript>
@@ -32,7 +44,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             src="https://www.googletagmanager.com/ns.html?id=GTM-W2CHMJNV"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"></iframe>
+            style={{ display: "none", visibility: "hidden" }}></iframe>
         </noscript>
         <Header />
         {children}
